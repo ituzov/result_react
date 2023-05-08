dbee52944616c96d20d328eaaef4e72b790168f4
ituzov <ilia@ituzov.ru>
2023-05-08 20:08:51 +0300

    JSX and NoJSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => (
+	//Создание функции наверное единственное место с императивным стилем далее все носит декларативный характер
+	<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<p>
+				Edit <code>src/App.js</code> and save to reloaded.
+			</p>
+			<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Learn React
+			</a>
+			<p>{new Date().getFullYear()}</p>
+		</header>
+	</div>
+);
 
-export default App;
+export const NoJSXApp = () => {
+	//Вариант без JSX
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('p', null, new Date().getFullYear()),
+		),
+	);
+};
